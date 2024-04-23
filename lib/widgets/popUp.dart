dba3fb70dfcfedcf08c2c5dc7dbdd8d09310ec14
Lucas Bustamante <lucascostabustamante@gmail.com>
2024-04-23 00:57:13 -0300

    update

--- lib/widgets/popUp.dart
+++ lib/widgets/popUp.dart
@@ -1,14 +1,62 @@
+import 'dart:async';
+
+import 'package:bitau/BLE/ble_controller.dart';
 import 'package:bitau/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:get/get.dart';
 
-class MeuPopup extends StatelessWidget {
+class MeuPopup extends StatefulWidget {
+
+
+  @override
+  State<MeuPopup> createState() => _MeuPopupState();
+}
+
+class _MeuPopupState extends State<MeuPopup> {
+
+  bool close = false;
+  int rssiValue = 0;
+  String agencia = "";
+  late Timer periodicTimer;
+  bool popupDisplayed = false;
+
+  _checkBluetoothResults(List<ScanResult> scanResults) {
+
+    for (var result in scanResults) {
+      if (result.device.name?.toLowerCase() == 'esp32 beacon test') {
+        rssiValue = result.rssi;
+        agencia = result.device.name;
+      }
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    Get.put(BleController());
+
+    // Inicia o timer para verificações periódicas
+    var periodicTimer = Timer.periodic(Duration(seconds: 3), (Timer timer) {
+      if (!popupDisplayed) {
+        Get.find<BleController>().scanDevices();
+      }
+    });
+
+    Get.find<BleController>().scanResults.listen((List<ScanResult> scanResults) {
+      _checkBluetoothResults(scanResults);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Olá, Lucas',style: TextStyle(
         color: kOrangeDarkColor
       ),),
-      content: Text('Acabei de notar que você está na agencia ',
+      content: Text('Acabei de notar que você está na agencia $agencia',
         style: TextStyle(
           fontSize: 15,
               color: Colors.grey.shade800
@@ -43,7 +91,7 @@ class MeuPopup extends StatelessWidget {
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                     minimumSize: Size(MediaQuery.of(context).size.width, 50),
-                   
+
                     shape: RoundedRectangleBorder(
 
                         borderRadius: BorderRadius.circular(10)
